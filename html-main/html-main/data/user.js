dac1007adf556b42b9de8c73ad6bfbe12325f110
HuyenNguyenVan <60337500+HuyenNguyenVan@users.noreply.github.com>
2024-06-04 16:14:13 +0700

    update login and registration

---
+++ html-main/html-main/data/user.js
@@ -0,0 +1,29 @@
+
+const DOCTOR = "DOCTOR";
+const USER = "USER";
+const ADMIN = "ADMIN";
+const USER_LOGINED_OR_NOT = "USER_LOGINED_OR_NOT";
+
+let peoples = [
+    {
+        name: "huyennv",
+        age: 12,
+        role : ADMIN
+    }
+]
+
+const BAC_SI_MUI = "BAC_SI_MUI"
+
+
+// const userInfo =
+// {
+//   username,
+//   password,
+//   role,
+//   lichKham: [
+//     {
+//         tenBacSy,
+//         thoiGianKham
+//     }
+//   ] 
+// };
---
+++ html-main/html-main/js/constVariable.js
@@ -0,0 +1,10 @@
+const ADMIN = "ADMIN";
+const USER = "USER";
+const DOCTOR = "DOCTOR";
+const USER_LOGINED_OR_NOT = "TOCKEN";
+
+// const userInfo = {
+//     email,
+//     password,
+//     role
+// }
